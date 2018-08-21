b1bb2967c7798800547212c3500a24388ac9f829
Aleksey A. Morozov <AlekVolsk@users.noreply.github.com>
2018-08-21 11:56:24 +0400

    Update adminextensionscripts.php

--- adminextensionscripts.php
+++ adminextensionscripts.php
@@ -6,20 +6,22 @@
  * @license     GNU General Public License version 3 or later; see http://www.gnu.org/licenses/gpl-3.0.txt
  */
 
-class JFormFieldAdminExtensionScripts extends JFormField
+class FormFieldAdminExtensionScripts extends Joomla\CMS\Form\FormField
 {
-	protected $type = 'AdminExtensionScripts';
+	protected $type = 'adminextensionscripts';
 
 	protected function getInput()
 	{
-		$path = str_replace('\\', '/', str_replace(JPATH_SITE, '', dirname(__FILE__)));
-		if ((int)$this->element['styles'] == true)
+		$path = str_replace( '\\', '/', str_replace( JPATH_SITE, '', dirname( __FILE__ ) ) );
+		
+		if ( (int) $this->element[ 'styles' ] == true )
 		{
-			JFactory::getDocument()->addStyleSheet($path . '/adminextensionscripts.css');
+			Joomla\CMS\Factory::getDocument()->addStyleSheet( $path . '/adminextensionscripts.css ');
 		}
-		if ((int)$this->element['script'] == true)
+		
+		if ( (int) $this->element[ 'script' ] == true )
 		{
-			JFactory::getDocument()->addScript($path . '/adminextensionscripts.js');
+			Joomla\CMS\Factory::getDocument()->addScript( $path . '/adminextensionscripts.js' );
 		}
 		
 		return '';
