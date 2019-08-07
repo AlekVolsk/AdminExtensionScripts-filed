c8d8ebd184c64e9d44301f615ad2b9327bf4cfd8
AlekVolsk <AlekVolsk@users.noreply.github.com>
2019-08-07 15:47:19 +0400

    Update adminextensionscripts.php

--- adminextensionscripts.php
+++ adminextensionscripts.php
@@ -1,29 +1,40 @@
 <?php defined('JPATH_PLATFORM') or die;
 /*
- * @package     Joomla.Site
- * @subpackage  Layout
- * @copyright   Copyright (C) 2018 Aleksey A. Morozov (AlekVolsk). All rights reserved.
+ * @package     Joomla.Field
+ * @subpackage  adminextensionscripts
+ * @copyright   Copyright (C) 2019 Aleksey A. Morozov (AlekVolsk). All rights reserved.
  * @license     GNU General Public License version 3 or later; see http://www.gnu.org/licenses/gpl-3.0.txt
  */
 
-class FormFieldAdminExtensionScripts extends Joomla\CMS\Form\FormField
-{
-	protected $type = 'adminextensionscripts';
+use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Form\FormField;
 
-	protected function getInput()
-	{
-		$path = str_replace( '\\', '/', str_replace( JPATH_SITE, '', dirname( __FILE__ ) ) );
-		
-		if ( (int) $this->element[ 'styles' ] == true )
-		{
-			Joomla\CMS\Factory::getDocument()->addStyleSheet( $path . '/adminextensionscripts.css ');
-		}
-		
-		if ( (int) $this->element[ 'script' ] == true )
-		{
-			Joomla\CMS\Factory::getDocument()->addScript( $path . '/adminextensionscripts.js' );
-		}
-		
-		return '';
-	}
+class FormFieldAdminExtensionScripts extends FormField
+{
+    protected $type = 'adminextensionscripts';
+
+    protected function getLabel()
+    {
+        return '';
+    }
+
+    protected function getInput()
+    {
+        if (Factory::getApplication()->isClient('administrator') === false) {
+            return '';
+        }
+
+        $path = str_replace('\\', '/', str_replace(JPATH_SITE, '', dirname(__FILE__)));
+
+        if ((bool) $this->element['styles']) {
+            HTMLHelper::stylesheet($path . '/adminextensionscripts.css', ['version' => 'auto']);
+        }
+
+        if ((bool) $this->element['script']) {
+            HTMLHelper::script($path . '/adminextensionscripts.js', ['version' => 'auto']);
+        }
+
+        return '';
+    }
 }
